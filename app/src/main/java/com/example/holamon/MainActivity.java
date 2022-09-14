15aef465387669c03baa5b18467c8427517c44a7
Hector <hectorvallveg@gmail.com>
2022-09-14 17:29:06 +0200

    Ac1

--- app/src/main/java/com/example/holamon/MainActivity.java
+++ app/src/main/java/com/example/holamon/MainActivity.java
@@ -3,17 +3,29 @@ package com.example.holamon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1.findViewById();
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
     }
+
+    public void click(){
+
+
+    }
+
+
+
 }
